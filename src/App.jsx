86036c6feadfdaa6f4d2a27d873673cfcd46317f
Lahiru Kalhara <lahirukalharadimbulkumbura@gmail.com>
2025-02-14 14:47:40 +0530

    Body Container 1 implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/HeaderComponent/Header'
 import hero from '../src/images/Picture_woman@1x.png'
+import card1 from '../src/images/Icon@1x.png'
+import card2 from '../src/images/Icon@1x (1).png'
+import card3 from '../src/images/Icon@1x (2).png'
+import card4 from '../src/images/Icon@1x (3).png'
+import card5 from '../src/images/Icon@1x (4).png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,9 +22,38 @@ function App() {
           <a href="">Join Altea Gym</a>
         </div>
       </div>
-      
+
       <div className='bodyContainer1'>
-        
+        <h2>Our Reason for Being</h2>
+        <p>How can we help you?</p>
+        <div className='cardContainer'>
+          <div className='card'>
+            <img src={card1} alt="" />
+            <h5>Providers<br></br>of happiness</h5>
+            <h6>Feel good about yourself</h6>
+          </div>
+          <div className='card'>
+            <img src={card2} alt="" />
+            <h5>Providers<br></br>of happiness</h5>
+            <h6>Feel good about yourself</h6>
+          </div>
+          <div className='card'>
+            <img src={card3} alt="" />
+            <h5>Providers<br></br>of happiness</h5>
+            <h6>Feel good about yourself</h6>
+          </div>
+          <div className='card'>
+            <img src={card4} alt="" />
+            <h5>Providers<br></br>of happiness</h5>
+            <h6>Feel good about yourself</h6>
+          </div>
+          <div className='card'>
+            <img src={card5} alt="" />
+            <h5>Providers<br></br>of happiness</h5>
+            <h6>Feel good about yourself</h6>
+          </div>
+          
+        </div>
       </div>
 
     </>
